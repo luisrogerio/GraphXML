4b7e0005fe3551a5806959ea0e5f945b69cf53ab
Luís Rogério Ventura Martins Filho <luisrogeriojf@hotmail.com>
2015-11-20 14:09:59 -0200

    Métodos de pesquisa de Nós

--- src/java/model/No.java
+++ src/java/model/No.java
@@ -5,6 +5,8 @@
  */
 package model;
 
+import java.util.List;
+
 /**
  *
  * @author LuísRogério
@@ -25,4 +27,12 @@ public class No {
         this.id = id;
     }
 
+    public static No getNoById(String id, List<No> nos) {
+        for (No no : nos) {
+            if (no.getId().equals(id)) {
+                return no;
+            }
+        }
+        return null;
+    }
 }
