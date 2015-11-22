a3fca5031e071d53d125ce0e257041a9f2862b1c
Luís Rogério Ventura Martins Filho <luisrogeriojf@hotmail.com>
2015-11-22 18:44:13 -0200

    Grafo.xml

--- web/js/scriptFormCreator.js
+++ web/js/scriptFormCreator.js
@@ -1,27 +1,32 @@
-$(function() {
+//SCRIPT PARA OCULTAR A MENSAGEM APOS 4S;
+$(document).ready(function () {
+    setTimeout("$('#mensagem').hide(500)", 4000);
+});
+//SCRIPT PARA CRIAR OS ELEMENTOS E O CARAI A 4;
+$(function () {
     var i = 1;
     var j = 1;
-    $('#adiconaCampo').live('click', function() {
+    $('#adiconaCampo').live('click', function () {
         $('<p><label for="no"><input type="text" id="no" size="20" name="nos" value="" placeholder="Insira nome do Nó" /></label> \n\
             <a href="#" id="removerNo">Remove</a></p>').appendTo(formulario);
         i++;
         return false;
     });
-    $('#adiconaAresta').live('click', function() {
+    $('#adiconaAresta').live('click', function () {
         $('<p><label for="no"><input type="text" id="aresta" size="20" name="arestas" value="" placeholder="A,B" /></label> \n\
             <a href="#" id="removerAresta">Remove</a></p>').appendTo(arestas);
         j++;
         return false;
     });
 
-    $('#removerNo').live('click', function() {
+    $('#removerNo').live('click', function () {
         if (i > 1) {
             $(this).parents('p').remove();
             i--;
         }
         return false;
     });
-    $('#removerAresta').live('click', function() {
+    $('#removerAresta').live('click', function () {
         if (j > 1) {
             $(this).parents('p').remove();
             j--;
